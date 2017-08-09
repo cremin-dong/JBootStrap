28b70f0c6a266bf81a146d37673f3897cffb2672
cremin <dongmm@reignwood.com>
2017-08-09 12:48:13 +0800

    优化、精简插件

--- code/src/main/resources/static/js/base.js
+++ code/src/main/resources/static/js/base.js
@@ -1,16 +1,37 @@
+var jbootstrap = {};
+
+
 $(function () {
+
+    jbootstrap.ready();
+
+});
+
+
+/**
+ * 初始化函数
+ */
+jbootstrap.ready = function () {
+
+    jbootstrap.pjaxSetting();
+
+
+}
+
+/**
+ * pjax设定
+ */
+jbootstrap.pjaxSetting = function () {
 
     //Pjax页面跳转
     $(document).pjax('a[data-pjax]', '#pjax-container', {fragment: '#pjax-container'})
 
     //Pjax页面跳转进度条
     $(document).on('pjax:start', function () {
-       debugger
         NProgress.start();
     });
     $(document).on('pjax:end', function (event) {
         $(window).resize();
         NProgress.done();
     });
-
-})
+}
